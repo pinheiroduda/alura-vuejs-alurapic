0f525942b86a72a464048965eb7c95fbd7965835
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-05 13:29:18 -0300

    ✨ feat: adds data binding using v-bind directive and v-for to iterate in photo array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <template>
-  <div id="app"></div>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="photo of photos">
+        <img :src="photo.url" :alt="photo.title" />
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      title: "Alurapic",
+      photos: [
+        {
+          url:
+            "https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+          title: "desk"
+        },
+        {
+          url:
+            "https://images.unsplash.com/photo-1649089475838-00ddaabd1c82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+          title: "green"
+        }
+      ]
+    };
+  }
+};
+</script>
 
 <style></style>
