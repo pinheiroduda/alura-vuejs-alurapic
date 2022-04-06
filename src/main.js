ab237d0404a805a2d1d230ebc7cee846ae58499b
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-06 10:38:05 -0300

    ➕ add: vue router dependency

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: "#app",
