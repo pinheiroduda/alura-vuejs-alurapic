44707b3e047b7b1c2e3c0f8aab0fceb95e8d5cb3
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-06 09:45:38 -0300

    ✨ feat: adds v-show and v-on directive with double click to show and hide images

--- src/components/shared/panel/Panel.vue
+++ src/components/shared/panel/Panel.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="panel">
-    <h2 class="panel-title">{{ title }}</h2>
-    <slot class="panel-content"></slot>
+    <h2 class="panel-title" @dblclick="visible = !visible">{{ title }}</h2>
+    <div class="panel-content" v-show="visible">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["title"]
+  props: ["title"],
+
+  data() {
+    return {
+      visible: true
+    };
+  }
 };
 </script>
 
