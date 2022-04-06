c74f09a1b52649c1603d1e24b8f6a34403bbfbfb
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-06 10:31:22 -0300

    ♻️ refactor: delete useless code in app file now that home component was created

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,9 @@
 <template>
-  <div class="body">
-    <h1 class="title">{{ title }}</h1>
-    <input
-      type="search"
-      class="filter"
-      placeholder="filtre as imagens pelo título"
-      @input="filter = $event.target.value"
-    />
-    <ul class="photo-list">
-      <li class="photo-list-item" v-for="photo of filteredPhotos" :key="photo">
-        <my-panel :title="photo.titulo">
-          <responsive-image :url="photo.url" :title="photo.title" />
-        </my-panel>
-      </li>
-    </ul>
-  </div>
+  <div class="body"></div>
 </template>
 
 <script>
-import Panel from "./components/shared/panel/Panel.vue";
-import ResponsiveImage from "./components/shared/responsive-image/ResponsiveImage.vue";
-
-export default {
-  components: {
-    "my-panel": Panel,
-    "responsive-image": ResponsiveImage
-  },
-
-  data() {
-    return {
-      title: "Alurapic",
-      photos: [],
-      filter: ""
-    };
-  },
-
-  computed: {
-    filteredPhotos() {
-      if (this.filter) {
-        let exp = new RegExp(this.filter.trim(), "i");
-        return this.photos.filter(photo => exp.test(photo.titulo));
-      } else {
-        return this.photos;
-      }
-    }
-  },
-
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(photos => (this.photos = photos));
-  }
-};
+export default {};
 </script>
 
 <style>
@@ -60,22 +11,5 @@ export default {
   font-family: Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
-}
-
-.title {
-  text-align: center;
-}
-
-.filter {
-  display: block;
-  width: 100%;
-}
-
-.photo-list {
-  list-style: none;
-}
-
-.photo-list-item {
-  display: inline-block;
 }
 </style>
