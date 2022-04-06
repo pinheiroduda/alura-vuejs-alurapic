e67fff1f595a522e7e1090b49612d0f7decfd426
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-06 14:47:32 -0300

    ⚡️ improve: replace tag 'a' for tag 'router link' to avoid page reload

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
   <div class="body">
     <nav>
       <ul class="menu-list">
-        <li><a href="/" class="menu-list-item">Home</a></li>
-        <li><a href="/register" class="menu-list-item">Cadastro</a></li>
+        <li>
+          <router-link to="/" class="menu-list-item">
+            Home
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/register" class="menu-list-item">
+            Cadastro
+          </router-link>
+        </li>
       </ul>
     </nav>
     <router-view />
@@ -26,7 +34,7 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 15px 0 50px;
+  padding: 0 50px;
   list-style: none;
 }
 
