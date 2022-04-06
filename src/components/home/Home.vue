ac1ec89b55ab6612049b42ff8a01b8317919e146
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-06 10:30:46 -0300

    ✨ feat: create home component

---
+++ src/components/home/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h1 class="title">{{ title }}</h1>
+    <input
+      type="search"
+      class="filter"
+      placeholder="filtre as imagens pelo título"
+      @input="filter = $event.target.value"
+    />
+    <ul class="photo-list">
+      <li class="photo-list-item" v-for="photo of filteredPhotos" :key="photo">
+        <my-panel :title="photo.titulo">
+          <responsive-image :url="photo.url" :title="photo.title" />
+        </my-panel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Panel from "../shared/panel/Panel.vue";
+import ResponsiveImage from "../shared/responsive-image/ResponsiveImage.vue";
+
+export default {
+  components: {
+    "my-panel": Panel,
+    "responsive-image": ResponsiveImage
+  },
+
+  data() {
+    return {
+      title: "Alurapic",
+      photos: [],
+      filter: ""
+    };
+  },
+
+  computed: {
+    filteredPhotos() {
+      if (this.filter) {
+        let exp = new RegExp(this.filter.trim(), "i");
+        return this.photos.filter(photo => exp.test(photo.titulo));
+      } else {
+        return this.photos;
+      }
+    }
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(photos => (this.photos = photos));
+  }
+};
+</script>
+
+<style>
+.title {
+  text-align: center;
+}
+
+.filter {
+  display: block;
+  width: 100%;
+}
+
+.photo-list {
+  list-style: none;
+}
+
+.photo-list-item {
+  display: inline-block;
+}
+</style>
