738e44b89e9bacc8cf5e51d4d094ddc199076348
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-06 08:34:16 -0300

    ✨ feat: creates panel component to organize page components

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
     <h1 class="title">{{ title }}</h1>
     <ul class="photo-list">
       <li class="photo-list-item" v-for="photo of photos" :key="photo">
-        <div class="panel">
-          <h2 class="panel-title">{{ photo.titulo }}</h2>
-          <div class="panel-content">
-            <img class="image" :src="photo.url" :alt="photo.titulo" />
-          </div>
-        </div>
+        <my-panel :title="photo.titulo">
+          <img class="image" :src="photo.url" :alt="photo.titulo" />
+        </my-panel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Panel from "./components/shared/panel/Painel.vue";
+
 export default {
+  components: {
+    "my-panel": Panel
+  },
+
   data() {
     return {
       title: "Alurapic",
@@ -49,26 +52,6 @@ export default {
 
 .photo-list-item {
   display: inline-block;
-}
-
-.panel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.panel .panel-title {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  text-transform: uppercase;
 }
 
 .image {
---
+++ src/components/shared/panel/Painel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="panel">
+    <h2 class="panel-title">{{ title }}</h2>
+    <slot class="panel-content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"]
+};
+</script>
+
+<style>
+.panel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.panel .panel-title {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  text-transform: uppercase;
+}
+</style>
