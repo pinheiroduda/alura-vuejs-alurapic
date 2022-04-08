5db1a454e72a23ce261bb8ac046cb1796eab13db
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-08 14:10:46 -0300

    ✨ feat: populate register component

--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -1,13 +1,65 @@
 <template>
-  <h2 class="centered">Cadastro</h2>
+  <div>
+    <h1 class="centered">Cadastro</h1>
+    <h2 class="centered"></h2>
+
+    <form>
+      <div class="control">
+        <label for="title">TÍTULO</label>
+        <input id="title" autocomplete="off" />
+      </div>
+
+      <div class="control">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" />
+        <responsive-image />
+      </div>
+
+      <div class="control">
+        <label for="description">DESCRIÇÃO</label>
+        <textarea id="description" autocomplete="off"></textarea>
+      </div>
+
+      <div class="centered">
+        <my-button label="GRAVAR" type="submit" />
+        <router-link to="/">
+          <my-button label="VOLTAR" type="button" />
+        </router-link>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import ResponsiveImage from "../shared/responsive-image/ResponsiveImage.vue";
+import Button from "../shared/button/Button.vue";
+
+export default {
+  components: {
+    "responsive-image": ResponsiveImage,
+    "my-button": Button
+  }
+};
 </script>
 
 <style scoped>
 .centered {
   text-align: center;
+}
+
+.control {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.control label {
+  display: block;
+  font-weight: bold;
+}
+
+.control label + input,
+.control textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
 }
 </style>
