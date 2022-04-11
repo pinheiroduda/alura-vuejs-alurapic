d191df255bc9e8a2073ae8cefbd23cb4312423fc
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-11 14:59:54 -0300

    ✨ feat: translate vee validate error messages to portuguese

--- src/main.js
+++ src/main.js
@@ -9,12 +9,20 @@ import VeeValidate from "vee-validate";
 import { routes } from "./routes";
 
 import "./directives/Transform";
+import msg from "./pt_BR";
 
 Vue.use(VueResource);
 Vue.http.options.root = "http://localhost:3000";
 Vue.use(VueRouter);
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+  locale: "pt_BR",
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
+});
 
 const router = new VueRouter({
   routes: routes,
