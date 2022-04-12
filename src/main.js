a2f7c9eba8426db99b310c6dd68b26d2d72f7070
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-04-12 08:41:41 -0300

    ✨ feat: import bootstrap

--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ import VeeValidate from "vee-validate";
 import { routes } from "./routes";
 
 import "./directives/Transform";
+
 import msg from "./pt_BR";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 Vue.use(VueResource);
 Vue.http.options.root = "http://localhost:3000";
